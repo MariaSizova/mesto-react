69e4f4db180a7e641011a135bb6290a985f5b3dc
Maria Sizova <sizova.maria.olegovna@yandex.ru>
2023-03-22 15:35:02 +0300

    fix: ошибки в наименовании файлов

--- src/utils/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__save-btn',
-  inactiveButtonClass: 'popup__button_invalid',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-};
